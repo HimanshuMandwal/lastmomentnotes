3f3d43454b340595bd1b244195bd9937b18547f1
dheeraj0802 <50199862+dheeraj0802@users.noreply.github.com>
2021-06-15 23:16:36 +0530

    Added Contact-us

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import Header from './components/header/header.component';
+import Contact from './components/contact/contact';
 
 function App() {
   return (
     <div>
       <Header/>
       <Homepage/>
+      <Contact/>
     </div>
 
   );
